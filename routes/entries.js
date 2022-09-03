fd38214136d2520ab6a6cdf9a7e272421151bf2f
Matt Connolly <81105533+matt-conn@users.noreply.github.com>
2022-09-03 11:09:06 -0400

    experimenting with codebase

--- routes/entries.js
+++ routes/entries.js
@@ -3,14 +3,14 @@ const router = express.Router()
 const entriesController = require('../controllers/entries') 
 const { ensureAuth } = require('../middleware/auth')
 
-router.get('/', ensureAuth, entriesController.getEntries)
+router.get('/', ensureAuth,entriesController.getEntries)
 
-router.post('/createEntry', entriesController.createEntry)
+router.post('/createEntry',entriesController.createEntry)
 
-router.put('/markComplete', entriesController.markComplete)
+// router.put('/markComplete',entriesController.markComplete)
 
-router.put('/markIncomplete', entriesController.markIncomplete)
+// router.put('/markIncomplete',entriesController.markIncomplete)
 
-router.delete('/deleteEntry', entriesController.deleteEntry)
+router.delete('/deleteEntry',entriesController.deleteEntry)
 
 module.exports = router
